6dc8a98b856bddb8e76fa79e8341664bb0021ca1
Muzkraft <no1z555@gmail.com>
2022-11-11 17:48:38 +0500

    Add Program

---
+++ Program/Program.cs
@@ -0,0 +1,11 @@
+﻿string[] array = {"qwe", "12345", ":-)", "Rus", "Geek brains","Homework", "1234", "-5", "qwerty", "asd"};
+Console.WriteLine(String.Join(", ", array));
+
+string[] result = {};
+
+for (int i = 0; i < array.Length; i++)
+{
+    result = Array.FindAll(array, i => i.Length <= 3).ToArray();       
+}
+
+Console.WriteLine(String.Join(", ", result));
